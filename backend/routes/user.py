fc377caf040b51e2486dba9d6a757345f19a43c4
Pranav Mishra <mishrapranav242@gmail.com>
2026-02-13 04:34:40 +0530

    added the login and registration model and added the login route

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -1,19 +1,38 @@
 from fastapi import APIRouter, HTTPException
 from database import users_collection
+from schemas.user_schema import UserRegister, UserResponse,UserLogin,Token
 
 router = APIRouter()
 
 
-@router.post("/register")
-async def register_user(user: dict):
-
-    existing_user = await users_collection.find_one({"email": user.get("email")})
+@router.post("/register", response_model=UserResponse)
+async def register_user(user: UserRegister):
+    # Check if user already exists
+    existing_user = await users_collection.find_one({"email": user.email})
     if existing_user:
         raise HTTPException(status_code=400, detail="Email already registered")
 
-    await users_collection.insert_one(user)
+    # In a real app, hash the password here before saving
+    user_data = user.dict()
+    
+    await users_collection.insert_one(user_data)
 
     return {
-        "message": "User registered successfully",
-        "role": user.get("role")
+        "username": user.username,
+        "email": user.email,
+        "role": user.role,
+        "message": "User registered successfully"
+    }
+@router.post('/login',response_model=UserResponse)
+async def login_user(user:UserLogin):
+    existing_user=await users_collection.find_one({"email":user.email})
+    if not existing_user:
+        raise HTTPException(status_code=400,detail="user not found")
+    if existing_user['password']!=user.password:
+        raise HTTPException(status_code=400,detail="invalid password")
+    return {
+        "username":existing_user['username'],
+        "email":existing_user['email'],
+        "role":existing_user['role'],
+        "message":"user logged in successfully"
     }
