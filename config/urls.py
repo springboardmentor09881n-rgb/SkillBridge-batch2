394762a9fe717d4d27a5cced4d77892fdfbd28d9
Chandru S <chandrustechie@gmail.com>
2026-02-12 10:00:14 +0530

    Implemented JWT authentication and secured profile API

--- config/urls.py
+++ config/urls.py
@@ -27,3 +27,12 @@ urlpatterns = [
     path('', RedirectView.as_view(url='/api/docs/', permanent=False)),
 ]
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns += [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
