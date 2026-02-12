4f1f6b3cb232014b12ad7a8b13806ee525cb86c4
Chandru S <chandrustechie@gmail.com>
2026-02-12 21:31:03 +0530

    Remove all files and folders

--- gauravbackend/database.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-
-client = MongoClient("mongodb://localhost:27017")
-
-db = client["milestone_db"]
-
-user_collection = db["users"]
-profile_collection = db["profiles"]
